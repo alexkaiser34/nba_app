13989535e14a02e3a7b025c778a123ca68b3573e
alexkaiser34 <alexkaiser@me.com>
2023-09-13 17:58:51 -0400

    Update navbar to use links instead of href to navigate
    Using href caused the whole page to rerender every time, which meant
    the entire app rerendered every time a navbar item was pressed

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { PersonCircle } from 'react-bootstrap-icons';
 import { NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function AppNavBar(){
 
@@ -15,43 +16,55 @@ function AppNavBar(){
     return (
         <Navbar sticky="top" expand="md" bg={themeColor} variant={themeColor}>
         <Container fluid>
-          <Navbar.Brand href="Home">NBA-App</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/Home">Home</Nav.Link>
-                    <Nav.Link href="/Scores">Scores</Nav.Link>
-                    <Nav.Link href="/Players">Players</Nav.Link>
-                    <Nav.Link href="/Teams">Teams</Nav.Link>
-                    <Nav.Link href="/Standings">Standings</Nav.Link>
-                </Nav>
-
-                <Form className="d-flex me-4">
-                <Button variant="outline-success">Search</Button>
-                    <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="ms-2 me-4"
-                    aria-label="Search"
-                    />
-                </Form>
-                <Nav className="d-flex flex-row align-items-baseline justify-content-center">
-                    <p className={`text-${textColor} me-3`}>Hello User</p>
-                    <NavDropdown
-                        title={
-                            <span>
-                                <PersonCircle fontVariant={themeColor} size={35} />
-                            </span>
-                        }
-                        align="end"
-                        menuVariant={themeColor}
-                        drop="down"
-                        id='collapsible-nav-dropdown'
-                        className='me-2'>
-
-                            <NavDropdown.Item href="/Settings">Settings</NavDropdown.Item>
-                        </NavDropdown>
-                </Nav>
+            <LinkContainer to="/Home">
+                <Navbar.Brand href="Home">NBA-App</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <LinkContainer to="/Home">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/Scores">
+                            <Nav.Link>Scores</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/Players">
+                            <Nav.Link>Players</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/Teams">
+                            <Nav.Link>Teams</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/Standings">
+                            <Nav.Link>Standings</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                    <Form className="d-flex me-4">
+                    <Button variant="outline-success">Search</Button>
+                        <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="ms-2 me-4"
+                        aria-label="Search"
+                        />
+                    </Form>
+                    <Nav className="d-flex flex-row align-items-baseline justify-content-center">
+                        <p className={`text-${textColor} me-3`}>Hello User</p>
+                        <NavDropdown
+                            title={
+                                <span>
+                                    <PersonCircle fontVariant={themeColor} size={35} />
+                                </span>
+                            }
+                            align="end"
+                            menuVariant={themeColor}
+                            drop="down"
+                            id='collapsible-nav-dropdown'
+                            className='me-2'>
+                                <LinkContainer to="/Settings">
+                                    <NavDropdown.Item>Settings</NavDropdown.Item>
+                                </LinkContainer>
+                            </NavDropdown>
+                    </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
